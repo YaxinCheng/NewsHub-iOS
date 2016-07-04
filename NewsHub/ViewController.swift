de6f5ca31c5cd9df672b18639c8df470b5404f70
WatashiJ <yx299612@dal.ca>
2016-07-04 15:52:54 -0300

    Initial Commit

---
+++ NewsHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsHub
+//
+//  Created by Yaxin Cheng on 2016-07-04.
+//  Copyright © 2016 Yaxin Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
